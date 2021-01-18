40367cd8e5d7806a268b6474d3e323d277261e82
drjofu <drjofu@web.de>
2021-01-18 12:33:14 +0100

    day 1

---
+++ FirstApp/FirstApp/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+  class Account
+  {
+    // Fields
+    private string name;
+    private int number;
+    private double balance;
+
+    // Properties
+
+    public string Name
+    {
+      get { return name; }
+      set { name = value; }
+    }
+
+    // ctor
+    public Account()
+    {
+      this.name = "undefined";
+      number = -999;
+    }
+
+    public Account(string name, int number, double balance)
+    {
+      this.name = name;
+      this.number = number;
+      this.balance = balance;
+    }
+
+    public override string ToString()
+    {
+      return $"Account {number} of {name}, balance: {balance}";
+    }
+  }
+}
--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using io = System.IO;
+
+using System;
 
 namespace FirstApp
 {
   class Program
   {
     static void Main(string[] args)
+    {
+      //Example1();
+
+      Account acc1 = new Account();
+      Account acc2 = new Account("Uncle Scrooge", 1, 99999999);
+
+      acc1.Name = "Micky Mouse";
+
+      Console.WriteLine(acc1);
+      Console.WriteLine(acc2);
+      Console.WriteLine(acc2.Name);
+      Console.ReadLine();
+    }
+
+    private static void Example1()
     {
       int x = 10;
       int y = 3;
@@ -78,12 +91,66 @@ namespace FirstApp
 
       Print("Hello world");
 
-      Console.ReadLine();
+      Console.WriteLine(Sum(3.4, 5.6));
+
+      Console.WriteLine(Sum(10, 4.5));
+
+      Console.WriteLine(Sum2());
+      Console.WriteLine(Sum2(1, 2));
+      Console.WriteLine(Sum2(1, 2, 3, 4, 5, 6));
+      Console.WriteLine(Sum2(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+      Console.WriteLine(Sum2(new int[] { 13, 45, 65, 7 }));
+
+      Console.WriteLine("f: {0}, xy: {4}", 1, 2, 3, 4, 5, 6);
+
+      Console.WriteLine("*********************************************");
+
+      DoSomething();
+      DoSomething("learn C#");
+      DoSomething("learn overloads", 15);
+      DoSomething(duration: 30);
+      DoSomething(duration: 40, what: "learn optional parameters");
+
+      Console.WriteLine("*********************************************");
+
+      DateTime now = DateTime.Now;
+      Console.WriteLine(now);
+      Console.WriteLine(DateTime.Today);
+      Console.WriteLine($"{now:dd.MMMM yyyy}");
+      Console.WriteLine($"{now:d}");
+      Console.WriteLine($"{now:D}");
+      Console.WriteLine($"{now:t}");
+      Console.WriteLine($"{now:T}");
+      Console.WriteLine($"{now:g}");
+      Console.WriteLine($"{now:G}");
+      Console.WriteLine(now.ToLongTimeString());
+      Console.WriteLine(now.ToUniversalTime().ToUniversalTime().ToUniversalTime().ToUniversalTime().ToLocalTime());
+
+      DateTime dt = DateTime.Parse("3.11.2004");
+
+      TimeSpan diff = dt - now;
+      TimeSpan diff2 = now - DateTime.Today;
+      Console.WriteLine(now + diff2);
+      TimeSpan diff3 = TimeSpan.FromDays(30);
+      Console.WriteLine(now + diff3);
+
+      Console.WriteLine();
+    }
+
+    static void DoSomething(string what = "nothing", int duration = 10)
+    {
+      Console.WriteLine($"Do {what} {duration} minutes");
+
     }
 
     static int Sum(int a, int b)
     {
       Console.WriteLine("hello from Sum");
+      return a + b;
+    }
+
+    static double Sum(double a, double b)
+    {
       return a + b;
     }
 
@@ -95,6 +162,17 @@ namespace FirstApp
       Console.WriteLine($"My text is {text} and x has the value {x:0.0}, x²: {x * x }"); // string interpolation (C#6.0)
 
 
+    }
+
+    static int Sum2(params int[] numbers)
+    {
+      int sum = 0;
+      foreach (int number in numbers)
+      {
+        sum += number;
+      }
+
+      return sum;
     }
   }
 }
